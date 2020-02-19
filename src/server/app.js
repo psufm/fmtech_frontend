a12efe9a660479f2783f70ced93226d60f747c74
seonguk_park <psufm@fmtech.io>
2020-02-19 14:24:13 +0900

    nodemon 추가
    추가작업 및 약식 수정

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 const app = express();
-import api from "./routes/index";
+import router from "./routes/index";
 
 app.use(bodyParser.json());
-app.use("/api", api);
+app.use("/login", router);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/", (req, res) => res.json({ data: "this is index." }));
+router.get("/", (req, res) => res.json({ data: "기본." }));
+router.get("/memberjoin", (req, res) => res.json({ data: "회원가입" }));
+router.get("/getdata", (req, res) => res.json({ data: "정보출력" }));
 
 module.exports = router;
